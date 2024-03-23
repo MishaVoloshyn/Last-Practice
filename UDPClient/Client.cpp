5ff337ba0062b06892c5304c2b76b465dfbe1e66
Maria Shevchenko <sevcenkomaria72@gmail.com>
2024-03-23 23:32:13 +0200

    Add project files.

---
+++ UDPClient/Client.cpp
@@ -0,0 +1,148 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <ws2tcpip.h>
+#include <windows.h>
+#include <iostream>
+#include <queue>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+#pragma comment (lib, "Ws2_32.lib")
+
+#define DEFAULT_BUFLEN 4096
+
+#define SERVER_IP "127.0.0.1"
+#define DEFAULT_PORT "8888"
+
+SOCKET client_socket;
+bool Wait = false;
+
+// Очередь для хранения сообщений от клиентов
+queue<string> messages_queue;
+
+bool isOrderInProgress = false;
+
+DWORD WINAPI Sender(void* client_socket_ptr)
+{
+
+    while (true)
+    {
+        char query[DEFAULT_BUFLEN];
+        cin.getline(query, DEFAULT_BUFLEN);
+        send(client_socket, query, strlen(query), 0);
+
+    }
+}
+DWORD WINAPI Receiver(void* param)
+{
+    while (true)
+    {
+        char response[DEFAULT_BUFLEN];
+        int result = recv(client_socket, response, DEFAULT_BUFLEN, 0);
+        if (result == SOCKET_ERROR) {
+            return 1;
+        }
+        response[result] = '\0';
+
+        int totalCookTime = 0;
+        double price = 0;
+
+        char* spacePos = strchr(response, ' '); 
+        bool isZero = false;
+
+        if (spacePos != nullptr)
+        {
+            *spacePos = '\0'; 
+            spacePos++; 
+
+            totalCookTime = atoi(response); 
+            price = atof(spacePos); 
+
+           
+            if (totalCookTime == 0 || price == 0)
+            {
+                isZero = true; 
+            }
+
+            if (totalCookTime > 0)
+            {
+                cout << "Время ожидания составит " << totalCookTime << " секунд, с вас - " << price << "$" << endl;
+            }
+            else
+            {
+                cout << "Ваш заказ готов!" << endl;
+            }
+
+            if (isZero)
+            {
+                cout << "Извините, данной позиции нет в меню. Пожалуйста, выберите что-то другое." << endl;
+            }
+            
+        }
+  
+    }
+
+}
+
+int main()
+{
+    system("title Client");
+    setlocale(LC_CTYPE, "Russian");
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0)
+    {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        return 1;
+    }
+
+    addrinfo hints = {};
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    addrinfo* result = nullptr;
+    iResult = getaddrinfo(SERVER_IP, DEFAULT_PORT, &hints, &result);
+    if (iResult != 0) {
+        printf("getaddrinfo failed with error: %d\n", iResult);
+        WSACleanup();
+        return 2;
+    }
+
+    addrinfo* ptr = nullptr;
+    for (ptr = result; ptr != NULL; ptr = ptr->ai_next)
+    {
+        client_socket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+        if (client_socket == INVALID_SOCKET) {
+            printf("socket failed with error: %ld\n", WSAGetLastError());
+            WSACleanup();
+            return 3;
+        }
+        iResult = connect(client_socket, ptr->ai_addr, (int)ptr->ai_addrlen);
+        if (iResult == SOCKET_ERROR) {
+            closesocket(client_socket);
+            client_socket = INVALID_SOCKET;
+            continue;
+        }
+        break;
+    }
+
+    freeaddrinfo(result);
+
+    if (client_socket == INVALID_SOCKET) {
+        printf("Unable to connect to server!\n");
+        WSACleanup();
+        return 4;
+    }
+
+    cout << "Добро пожаловать в наше кафе! Что желаете заказать?" << endl;
+    CreateThread(0, 0, Sender, 0, 0, 0);
+    CreateThread(0, 0, Receiver, 0, 0, 0);
+
+    Sleep(INFINITE);
+
+    return 0;
+}
---
+++ UDPExample/Server.cpp
@@ -0,0 +1,179 @@
+﻿#include <winsock2.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cctype>
+#include <sstream>
+
+#include <string>
+
+using namespace std;
+
+#define MAX_CLIENTS 30
+#define DEFAULT_BUFLEN 4096
+
+#pragma comment(lib, "ws2_32.lib") 
+#pragma warning(disable:4996) 
+
+SOCKET server_socket;
+
+struct MenuItem {
+    string name;
+    double price;
+    int cookTime; 
+};
+
+map<string, MenuItem> menu = {
+    {"burger", {"burger", 2.50, 5}},
+    {"tea", {"tea", 1.25, 1}},
+    {"coffee", {"coffee", 1.50, 1}},
+    {"pancake", {"pancake", 2.20, 2}},
+    {"sprite", {"sprite", 1.15, 1}},
+    {"cola", {"cola", 1.25, 1}},
+    {"sandwich", {"sandwich", 2.45, 2}}
+};
+
+vector<pair<string, int>> orders_queue; // Очередь заказов 
+
+int main() {
+    system("title Server");
+
+    puts("Start server... DONE.");
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        printf("Failed. Error Code: %d", WSAGetLastError());
+        return 1;
+    }
+
+    // create a socket
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+        printf("Could not create socket: %d", WSAGetLastError());
+        return 2;
+    }
+
+    // prepare the sockaddr_in structure
+    sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(8888);
+
+    // bind socket
+    if (bind(server_socket, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR) {
+        printf("Bind failed with error code: %d", WSAGetLastError());
+        return 3;
+    }
+
+    listen(server_socket, MAX_CLIENTS);
+
+    puts("Server is waiting for incoming connections...\nPlease, start one or more client-side app.");
+
+    fd_set readfds;
+    SOCKET client_socket[MAX_CLIENTS] = {};
+    int currentClientIndex = 0;
+    while (true) 
+    {
+        FD_ZERO(&readfds);
+        FD_SET(server_socket, &readfds);
+
+        for (int i = 0; i < MAX_CLIENTS; i++) 
+        {
+            SOCKET s = client_socket[i];
+            if (s > 0) {
+                FD_SET(s, &readfds);
+                currentClientIndex = i;
+            }
+        }
+
+        if (select(0, &readfds, NULL, NULL, NULL) == SOCKET_ERROR) 
+        {
+            printf("select function call failed with error code : %d", WSAGetLastError());
+            return 4;
+        }
+
+        SOCKET new_socket;
+        sockaddr_in address;
+        int addrlen = sizeof(sockaddr_in);
+        if (FD_ISSET(server_socket, &readfds)) {
+            if ((new_socket = accept(server_socket, (sockaddr*)&address, &addrlen)) < 0) {
+                perror("accept function error");
+                return 5;
+            }
+
+            cout << "New client connected" << endl;
+
+            for (int i = 0; i < MAX_CLIENTS; i++) 
+            {
+                if (client_socket[i] == 0) {
+                    client_socket[i] = new_socket;
+                    break;
+                }
+            }
+        }
+
+        int t = 0;
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            SOCKET s = client_socket[i];
+           
+            if (FD_ISSET(s, &readfds)) {
+                char client_message[DEFAULT_BUFLEN];
+
+                int client_message_length = recv(s, client_message, DEFAULT_BUFLEN, 0);
+                client_message[client_message_length] = '\0';
+                string message = client_message;
+                
+                // Обработка заказа
+                istringstream stream(message);
+                string word;
+                vector<string> items;
+
+                for (int i = 0; i < menu.size(); i++)
+                {
+                    while (stream >> word) 
+                    {
+                        items.push_back(word);
+                    }
+                }
+                
+                int totalCookTime = 0;
+                double totalPrice = 0;
+                for (const string& item : items) 
+                {
+                    // проверка на наличие позиции
+                    auto it = menu.find(item);
+                    if (it != menu.end()) 
+                    {
+                        totalCookTime += it->second.cookTime;
+                        totalPrice += it->second.price;
+                        orders_queue.push_back({ item, it->second.cookTime });
+                    }
+                }
+                t = totalCookTime;
+                // Отправляем информацию о времени и цене
+                string mes = to_string(totalCookTime) + " " + to_string(totalPrice);
+                send(s, mes.c_str(), mes.length(), 0);
+                //send(s, to_string(totalCookTime).c_str(), sizeof(totalCookTime), 0);   
+  
+            }
+        }
+
+        // Обработка заказов в очереди
+        string ready = "Your order is ready!";
+        if (!orders_queue.empty()) 
+        {
+            cout << "Preparing order: ";
+            for (int i = 0; i < orders_queue.size(); i++)
+            {
+                cout << orders_queue[i].first << " ";
+            }
+            cout << endl;
+
+            //в миллесекундах 
+            Sleep(t * 1000);
+            send(client_socket[currentClientIndex], ready.c_str(), 100, 0);
+            orders_queue.erase(orders_queue.begin()); 
+            cout << "Order is ready" << endl;
+        }
+    }
+
+    WSACleanup();
+}
